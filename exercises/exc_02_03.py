c22343acdf07d6da259614b6282a9e1d64716991
Anastasiia Generalova <rhinrei@gmail.com>
2019-05-10 17:46:58 +0300

    sklearn

--- exercises/exc_02_03.py
+++ exercises/exc_02_03.py
@@ -3,4 +3,4 @@ pd.set_option('display.expand_frame_repr', False)
 
 tweets_csv = pd.read_csv("exercises/tweets.csv")
 tweets_df = pd.DataFrame(tweets_csv)
-print(tweets_df.iloc[:, 1:4])
+print(tweets_df.____)
--- exercises/solution_02_04.py
+++ exercises/solution_02_04.py
@@ -3,4 +3,4 @@ pd.set_option('display.expand_frame_repr', False)
 
 tweets_csv = pd.read_csv("exercises/tweets.csv")
 tweets_df = pd.DataFrame(tweets_csv)
-print(tweets_df.iloc[:, 1:4])
+tweets_df = tweets_df.dropna(axis=0)
