f794fb08c654c3d042f9cf4f84edc95f3ac2d765
Anastasiia Generalova <rhinrei@gmail.com>
2019-05-10 22:08:24 +0300

    na

--- exercises/test_02_04.py
+++ exercises/test_02_04.py
@@ -1,5 +1,5 @@
 def test():
     assert "dropna" in __solution__, "Вы удаляете пустые значения?"
-    assert test == len(tweets_df), "Кажется, вы не удалили пустые значения"
+    # assert test == len(tweets_df), "Кажется, вы не удалили пустые значения"
 
     __msg__.good("Well done!")
